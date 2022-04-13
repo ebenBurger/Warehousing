8f9978c445945fbeee57d2764bf81c57046bc139
ebenBurger <99735780+ebenBurger@users.noreply.github.com>
2022-04-13 16:40:55 +0200

    migration - updated cargo table

---
+++ warehouse-Report/WarehouseReport.Api/Migrations/20220413135446_CargoTableModelUpdated.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WarehouseReport.Api.Migrations
+{
+    public partial class CargoTableModelUpdated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CurrentDate",
+                table: "Cargo",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<double>(
+                name: "DollarRate",
+                table: "Cargo",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StorageInvoiceDate",
+                table: "Cargo",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CurrentDate",
+                table: "Cargo");
+
+            migrationBuilder.DropColumn(
+                name: "DollarRate",
+                table: "Cargo");
+
+            migrationBuilder.DropColumn(
+                name: "StorageInvoiceDate",
+                table: "Cargo");
+        }
+    }
+}
