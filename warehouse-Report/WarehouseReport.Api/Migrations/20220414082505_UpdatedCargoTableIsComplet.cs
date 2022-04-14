1c0143f25ed5d70beed0474acf76fa373ac7674b
ebenBurger <99735780+ebenBurger@users.noreply.github.com>
2022-04-14 13:36:54 +0200

    migrate added

---
+++ warehouse-Report/WarehouseReport.Api/Migrations/20220414082505_UpdatedCargoTableIsComplet.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WarehouseReport.Api.Migrations
+{
+    public partial class UpdatedCargoTableIsComplet : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsComplete",
+                table: "Cargo",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsComplete",
+                table: "Cargo");
+        }
+    }
+}
---
+++ warehouse-Report/WarehouseReport.Api/Migrations/20220414092909_TypoFixedCon.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WarehouseReport.Api.Migrations
+{
+    public partial class TypoFixedCon : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "KgCBMConvertion",
+                table: "Cargo",
+                newName: "KgCBMConversion");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "KgCBMConversion",
+                table: "Cargo",
+                newName: "KgCBMConvertion");
+        }
+    }
+}
