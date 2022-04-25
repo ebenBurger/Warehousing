599f87e1b4c4c850fe758fd4d3609deb2dd2b267
ebenBurger <99735780+ebenBurger@users.noreply.github.com>
2022-04-25 16:00:31 +0200

    container model cols updated

--- warehouse-Report/WarehouseReport.Api/Models/ContainerModel.cs
+++ warehouse-Report/WarehouseReport.Api/Models/ContainerModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace WarehouseReport.Api.Models
@@ -10,6 +11,23 @@ namespace WarehouseReport.Api.Models
         [Key]
         public int ContainerId { get; set; }
         public string ContainerName { get; set; }
+        
+        public bool GP20ft { get; set; }
+        public bool FRIG20ft { get; set; }
+        public bool FROOG20ft { get; set; }
+        public bool TOIG20ft { get; set; }
+        public bool OTOOG20ft { get; set; }
+        public bool GP40ft { get; set; }
+        public bool HQ40ft { get; set; }
+        public bool FRIG40ft { get; set; }
+        public bool FROOG40ft { get; set; }
+        public bool OTIG40ft { get; set; }
+        public bool OTOOG40ft { get; set; }
+
+        public string SealOne { get; set; }
+        public string SealTwo { get; set; }
+        public DateTime PackingDate { get; set; }
+        public string PackingLocation { get; set; }
         public bool IsComplete { get; set; }
         public DateTime DateCreated { get; set; }
         public DateTime? DateCompleted { get; set; }
