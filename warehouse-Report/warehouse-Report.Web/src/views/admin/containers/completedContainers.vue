8c70f0c14a49a75ae321be4c101dd4a5183c1f9d
ebenBurger <99735780+ebenBurger@users.noreply.github.com>
2022-04-28 15:00:52 +0200

    complete Container and cargo UI added

---
+++ warehouse-Report/warehouse-Report.Web/src/views/admin/containers/completedContainers.vue
@@ -0,0 +1,49 @@
+﻿<template>
+    <div>
+        <b-row>
+            <b-col>
+                <div class="d-flex justify-content-between align-items-center">
+                    <h4 class="m-0">Warehouse Report</h4>
+                </div>
+                <b-col class="text-right">
+                    <b-button variant="outline-primary" size="sm" squared @click="goBack">
+                        Back
+                    </b-button>
+                </b-col>
+            </b-col>
+        </b-row>
+        <b-row class="text-center">
+            <b-col>
+                <h1>Comming Soon</h1>
+            </b-col>
+        </b-row>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({}),
+    beforeCreate() {
+    },
+    created() {
+    },
+    beforeMount() {
+    },
+    mounted() {
+    },
+    beforeUpdate() {
+    },
+    updated() {
+    },
+    methods: {
+        goBack() {
+            this.$router.back()
+        },
+    },
+    computed: {},
+}
+</script>
+
+<style scoped>
+
+</style>
