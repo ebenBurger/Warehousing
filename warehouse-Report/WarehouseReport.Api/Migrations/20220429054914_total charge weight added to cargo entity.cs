840cb26f1aed4eefaa2105e146c8ebd18753bd0e
ebenBurger <99735780+ebenBurger@users.noreply.github.com>
2022-04-29 10:52:15 +0200

    migration updated

---
+++ warehouse-Report/WarehouseReport.Api/Migrations/20220429054914_total charge weight added to cargo entity.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WarehouseReport.Api.Migrations
+{
+    public partial class totalchargeweightaddedtocargoentity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "TotalChargeableWeight",
+                table: "Cargo",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalChargeableWeight",
+                table: "Cargo");
+        }
+    }
+}
