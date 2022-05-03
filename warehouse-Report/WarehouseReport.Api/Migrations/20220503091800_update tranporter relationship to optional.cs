f9e5c4e36617efdb4cc1f61748032b86fbe45177
ebenBurger <99735780+ebenBurger@users.noreply.github.com>
2022-05-03 12:32:04 +0200

    migration updated

---
+++ warehouse-Report/WarehouseReport.Api/Migrations/20220503091800_update tranporter relationship to optional.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WarehouseReport.Api.Migrations
+{
+    public partial class updatetranporterrelationshiptooptional : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Package_Transporter_TransporterId",
+                table: "Package");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Package_TransporterId",
+                table: "Package");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TransporterId",
+                table: "Package",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Package_TransporterId",
+                table: "Package",
+                column: "TransporterId",
+                unique: true,
+                filter: "[TransporterId] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Package_Transporter_TransporterId",
+                table: "Package",
+                column: "TransporterId",
+                principalTable: "Transporter",
+                principalColumn: "TransporterId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Package_Transporter_TransporterId",
+                table: "Package");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Package_TransporterId",
+                table: "Package");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TransporterId",
+                table: "Package",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Package_TransporterId",
+                table: "Package",
+                column: "TransporterId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Package_Transporter_TransporterId",
+                table: "Package",
+                column: "TransporterId",
+                principalTable: "Transporter",
+                principalColumn: "TransporterId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ warehouse-Report/WarehouseReport.Api/Migrations/20220503093636_update container entiy.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WarehouseReport.Api.Migrations
+{
+    public partial class updatecontainerentiy : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ContainerName",
+                table: "Container",
+                newName: "Lading");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ContainerNumber",
+                table: "Container",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FileReference",
+                table: "Container",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContainerNumber",
+                table: "Container");
+
+            migrationBuilder.DropColumn(
+                name: "FileReference",
+                table: "Container");
+
+            migrationBuilder.RenameColumn(
+                name: "Lading",
+                table: "Container",
+                newName: "ContainerName");
+        }
+    }
+}
