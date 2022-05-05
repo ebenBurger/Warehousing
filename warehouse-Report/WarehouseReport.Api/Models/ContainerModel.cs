006915633df0fe32f2aa2aca23c2506e28b6962b
ebenBurger <99735780+ebenBurger@users.noreply.github.com>
2022-05-05 14:36:03 +0200

    container model updated

--- warehouse-Report/WarehouseReport.Api/Models/ContainerModel.cs
+++ warehouse-Report/WarehouseReport.Api/Models/ContainerModel.cs
@@ -21,6 +21,7 @@ namespace WarehouseReport.Api.Models
         public string Vesel { get; set; }
         public string Voyage { get; set; }
         public string Port { get; set; }
+        public string Status { get; set; }
         public bool IsComplete { get; set; }
         public DateTime DateCreated { get; set; }
         public DateTime? DateCompleted { get; set; }
